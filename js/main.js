39b891fe65f392a299b1c6e952f9fdbb937743ee
Diego <die.11.91@gmail.com>
2020-07-29 23:42:14 -0300

    Se modifico funcion sumar

--- js/main.js
+++ js/main.js
@@ -1,24 +1,6 @@
 let buttons = document.querySelectorAll("input[type='button']"); //Obtenemos todos los botones
 let numberDisplay = ''; //Almacenar los valores del display
 
-//Muestra los valores en display
-function displayNumber(number) {
-    document.getElementById('display').value = numberDisplay + number;
-    numberDisplay = document.getElementById('display').value;
-    console.log("displayNumber -> numberDisplay", numberDisplay)
-}
-
-//Sumar los valores 
-//TODO Arreglar funcion suma
-function sumNumbers(number) {
-    let sumResult = parseInt(numberDisplay) + number;
-    document.getElementById('display').value += sumResult;
-    console.log("sumNumbers -> sumResult", sumResult)
-    numberDisplay = '';
-    document.getElementById('display').value = '';
-
-}
-
 //Asignamos un evento clic a todos los botones
 for (let i = 0; i < buttons.length; i++) {
 
@@ -26,20 +8,45 @@ for (let i = 0; i < buttons.length; i++) {
     if (buttons[i].value != '+' &&
         buttons[i].value != '-' &&
         buttons[i].value != '/' &&
-        buttons[i].value != '*' && buttons[i].value != '=') {
-        
+        buttons[i].value != '*' &&
+        buttons[i].value != '=') {
+
         buttons[i].addEventListener("click", function (e) {
             e.preventDefault();
             displayNumber(this.value);
-    
+
         });
+
     } else if (buttons[i].value == '+') {
+
         buttons[i].addEventListener("click", function (e) {
             e.preventDefault();
             sumNumbers(numberDisplay);
         });
     }
 }
+
+//Muestra los valores en display
+function displayNumber(number) {
+    numberDisplay = numberDisplay + number;
+    document.getElementById('display').value = numberDisplay;
+    console.log("displayNumber -> numberDisplay", numberDisplay);
+    console.log("displayNumber -> numberDisplay", isNaN(numberDisplay));
+}
+
+//Sumar los valores 
+//TODO Arreglar funcion suma
+function sumNumbers(number) {
+    let sumResult = parseInt(numberDisplay) + parseInt(number);
+    sumResult = parseInt(sumResult);
+    document.getElementById('display').value += sumResult;
+    console.log("sumNumbers -> sumResult", sumResult)
+    console.log("sumNumbers -> sumResult", isNaN(sumResult))
+    numberDisplay = '';
+    document.getElementById('display').value = '';
+
+}
+
 
 
 
