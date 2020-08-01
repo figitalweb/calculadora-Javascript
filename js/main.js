ab5771cb209ca6e7d8a7d64d14798c2d4e20a5b8
Diego <die.11.91@gmail.com>
2020-07-31 21:13:06 -0300

    Se arreglo funcion suma

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 let buttons = document.querySelectorAll("input[type='button']"); //Obtenemos todos los botones
 let numberDisplay = ''; //Almacenar los valores del display
+let acumulado = 0;
 
 //Asignamos un evento clic a todos los botones
 for (let i = 0; i < buttons.length; i++) {
@@ -11,39 +12,34 @@ for (let i = 0; i < buttons.length; i++) {
         buttons[i].value != '*' &&
         buttons[i].value != '=') {
 
-        buttons[i].addEventListener("click", function (e) {
-            e.preventDefault();
+        buttons[i].addEventListener("click", function () {
             displayNumber(this.value);
 
         });
 
     } else if (buttons[i].value == '+') {
 
-        buttons[i].addEventListener("click", function (e) {
-            e.preventDefault();
-            sumNumbers(numberDisplay);
+        buttons[i].addEventListener("click", function () {
+            sumNumbers();
         });
     }
 }
 
 //Muestra los valores en display
 function displayNumber(number) {
-    numberDisplay = numberDisplay + number;
-    document.getElementById('display').value = numberDisplay;
+    document.getElementById('display').value = numberDisplay + number;
+    numberDisplay = document.getElementById('display').value;
     console.log("displayNumber -> numberDisplay", numberDisplay);
-    console.log("displayNumber -> numberDisplay", isNaN(numberDisplay));
 }
 
 //Sumar los valores 
 //TODO Arreglar funcion suma
-function sumNumbers(number) {
-    let sumResult = parseInt(numberDisplay) + parseInt(number);
-    sumResult = parseInt(sumResult);
-    document.getElementById('display').value += sumResult;
-    console.log("sumNumbers -> sumResult", sumResult)
-    console.log("sumNumbers -> sumResult", isNaN(sumResult))
+function sumNumbers() {
+
+    acumulado = acumulado + parseInt(numberDisplay);
     numberDisplay = '';
-    document.getElementById('display').value = '';
+    document.getElementById('display').value = acumulado;
+    console.log("sumNumbers -> sumResult", acumulado)
 
 }
 
